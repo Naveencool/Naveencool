d5c891567305f3c2b1fb298220b275c2b56643b9
Naveencool <99196456+Naveencool@users.noreply.github.com>
2022-02-10 20:51:12 +0530

    indexing class

---
+++ class_indexing.py
@@ -0,0 +1,23 @@
+# indexing of a string
+
+str5 = "Introduction"
+ind_of_d = str5[5]
+ind_of_I = str5[0]
+rev_of_n = str5[-1]
+rev_of_r = str5[-9]
+print("Element in fifth position is {}".format(ind_of_d))
+print("Element in in zeroth position is {}".format(ind_of_I))
+print("Element in -1th position is {}".format(rev_of_n))
+print("Element in -9th position is {}".format(rev_of_r))
+
+str6 = "Malayalam"
+ind_of_y = str6[4]
+ind_of_l = str6[6]
+ind_of_a = str6[1]
+rev_of_y = str6[-5]
+rev_of_m = str6[-9]
+print("element in fourth position is {}".format(ind_of_y))
+print("element in sixsth position is {}".format(ind_of_l))
+print("element in 1st position is {}".format(ind_of_a))
+print("element in -5th position is {}".format(rev_of_y))
+print("element in -9th position is {}".format(rev_of_m))
